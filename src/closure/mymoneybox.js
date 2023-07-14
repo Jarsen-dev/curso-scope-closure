93178d226f0b42172b59467f8d186ecd4d9ad22e
Armando <joseaat17@gmail.com>
2023-07-14 16:54:42 -0600

    Closure Example

---
+++ src/closure/mymoneybox.js
@@ -0,0 +1,22 @@
+// function myMoneyBox(coins) {
+//     let saveCoins = 0;
+//     saveCoins += coins;
+//     console.log(`MoneyBox: $${saveCoins}`);
+// }
+
+// myMoneyBox(5);
+// myMoneyBox(5);
+
+function moneyBox() {
+    let saveCoins = 0;
+    function countCoins(coins) {
+        saveCoins += coins;
+        console.log(`MoneyBox: $${saveCoins}`);
+    }
+    return countCoins;
+}
+
+const myMoneyBox = moneyBox();
+myMoneyBox(5);
+myMoneyBox(5);
+myMoneyBox(15);
